e679fff6d87ef46932413d5367dbc0dbc9855425
Marco Ugrotto <marco.ugrotto@gmail.com>
2021-11-11 11:47:06 +0100

    aggiornamento orderby

---
+++ EpicEnergyServices/EpicEnergyServices/src/main/java/it/epicode/beservice/model/Erole.java
@@ -0,0 +1,6 @@
+package it.epicode.beservice.model;
+
+public enum Erole {
+	ROLE_USER, ROLE_ADMIN;
+
+}
---
+++ EpicEnergyServices/EpicEnergyServices/src/main/java/it/epicode/beservice/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package it.epicode.beservice.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.epicode.beservice.model.User;
+import it.epicode.beservice.repository.UserRepository;
+import it.epicode.beservice.security.UserDetailsImpl;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUsername(username);
+		if (user.isPresent()) {
+			return UserDetailsImpl.build(user.get());
+		} else {
+			throw new UsernameNotFoundException("User Not Found with username: " + username);
+		}
+	}
+
+}
